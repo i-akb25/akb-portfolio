546c7d930704ab04e81283607d2b5e184cae37e8
ANURAG ARYAN <aryananurag1025@gmail.com>
2025-01-22 19:20:45 +0530

    fixed project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import Meta from "./components/Meta/Meta";
+import { FlatCompat } from '@eslint/eslintrc'
 
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.component.Meta,
+})
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
+
+export default eslintConfig
